867ada14ac78537131eca68f2aff11b9721511a3
tsabunkar <tsabunkar@gmail.com>
2020-07-05 15:50:32 +0530

    re-code

--- result/views/app.js
+++ result/views/app.js
@@ -1,37 +1,39 @@
-var app = angular.module('catsvsdogs', []);
-var socket = io.connect({transports:['polling']});
+var app = angular.module("catsvsdogs", []);
+var socket = io.connect({ transports: ["polling"] });
 
-var bg1 = document.getElementById('background-stats-1');
-var bg2 = document.getElementById('background-stats-2');
+var bg1 = document.getElementById("background-stats-1");
+var bg2 = document.getElementById("background-stats-2");
 
-app.controller('statsCtrl', function($scope){
+app.controller("statsCtrl", function ($scope) {
   $scope.aPercent = 50;
   $scope.bPercent = 50;
 
-  var updateScores = function(){
-    socket.on('scores', function (json) {
-       data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+  var updateScores = function () {
+    socket.on("scores", function (json) {
+      console.log("scores recieved from backend to UI", json);
+      data = JSON.parse(json);
+      var a = parseInt(data.a || 0);
+      var b = parseInt(data.b || 0);
 
-       var percentages = getPercentages(a, b);
+      var percentages = getPercentages(a, b);
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
+      bg1.style.width = percentages.a + "%";
+      bg2.style.width = percentages.b + "%";
 
-       $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
-       });
+      $scope.$apply(function () {
+        $scope.aPercent = percentages.a;
+        $scope.bPercent = percentages.b;
+        $scope.total = a + b;
+      });
     });
   };
 
-  var init = function(){
-    document.body.style.opacity=1;
+  var init = function () {
+    document.body.style.opacity = 1;
     updateScores();
   };
-  socket.on('message',function(data){
+  socket.on("message", function (data) {
+    console.log("message socker on", data);
     init();
   });
 });
@@ -40,11 +42,12 @@ function getPercentages(a, b) {
   var result = {};
 
   if (a + b > 0) {
-    result.a = Math.round(a / (a + b) * 100);
+    result.a = Math.round((a / (a + b)) * 100);
     result.b = 100 - result.a;
   } else {
     result.a = result.b = 50;
   }
 
+  console.log("result value is ", result);
   return result;
 }
